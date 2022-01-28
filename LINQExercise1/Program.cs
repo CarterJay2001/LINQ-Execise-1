c3d8fd8c04ec17d30ea58f7be9d670420c4d7a55
Jaylon Carter <carterjay2001@outlook.com>
2022-01-28 11:30:27 -0600

    initial commit

---
+++ LINQExercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LINQExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>(){
+            "God of War",
+            "Detroit: Become Human",
+            "Paper Mario",
+            "Watch Dogs 2",
+            "Star Wars Battlefront II",
+            "Rocket League",
+            "Yakuza Kiwami",
+            "Cuphead",
+            "Red Dead Redemption 2",
+            "Super Mario All-Stars" };
+
+            var lengthOrder = games.OrderByDescending(x => x.Length);
+            var longerThen6 = games.Where(x => x.Length > 6);
+        }
+    }
+}
